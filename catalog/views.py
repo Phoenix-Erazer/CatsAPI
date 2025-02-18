4c04f8c28303c42e8912a718b42c0a5080261df6
Phoen <Phoenixerazer@gmail.com>
2025-02-18 18:10:05 +0200

    add CatImageViewSet in views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from catalog.models import CatImage
+from catalog.serializers import CatImageSerializer
+
+
+class CatImageViewSet(viewsets.ModelViewSet):
+    queryset = CatImage.objects.all().order_by("-uploaded_at")
+    serializer_class = CatImageSerializer
