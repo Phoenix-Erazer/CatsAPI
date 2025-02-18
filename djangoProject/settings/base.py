bd27beca69e4bfb74a732b2b2cbb0f297a82e948
Phoen <Phoenixerazer@gmail.com>
2025-02-18 20:41:33 +0200

    Preparing the project for deployment

--- djangoProject/settings/base.py
+++ djangoProject/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-03^8#+i=$zo9%3e-%a$t5ld4ja-18qm34$y%yr8^@axp4^xl$)"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-03^8#+i=$zo9%3e-%a$t5ld4ja-18qm34$y%yr8^@axp4^xl$")
 
 
 # Application definition
@@ -74,13 +69,6 @@ WSGI_APPLICATION = "djangoProject.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
