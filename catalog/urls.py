5dccbdd736a172cd477d39d4a6de5087337de2a1
Phoen <Phoenixerazer@gmail.com>
2025-02-22 18:52:23 +0200

    fix code

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from django.urls.conf import include
 from rest_framework.routers import DefaultRouter
 
-from catalog.views import CatImageViewSet, proxy_cats_api
+from catalog.views import CatImageViewSet
 
 router = DefaultRouter()
 router.register(r"cats", CatImageViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path('api/proxy/cats/', proxy_cats_api),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,4 @@
 from rest_framework import viewsets
-import requests
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from catalog.models import CatImage
 from catalog.serializers import CatImageSerializer
 
@@ -9,14 +6,3 @@ from catalog.serializers import CatImageSerializer
 class CatImageViewSet(viewsets.ModelViewSet):
     queryset = CatImage.objects.all().order_by("-uploaded_at")
     serializer_class = CatImageSerializer
-
-
-@csrf_exempt
-def proxy_cats_api(request):
-    api_url = 'https://cats-api-hfgi.onrender.com/api/cats/'
-
-    try:
-        response = requests.get(api_url)
-        return JsonResponse(response.json(), safe=False)
-    except requests.exceptions.RequestException as e:
-        return JsonResponse({'error': str(e)}, status=500)
