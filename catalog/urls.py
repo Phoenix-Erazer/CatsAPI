d21b708ca47e0a9575e34c26a0891f06873d8667
Phoen <Phoenixerazer@gmail.com>
2025-02-22 18:27:18 +0200

    fix code

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.urls.conf import include
 from rest_framework.routers import DefaultRouter
 
-from catalog.views import CatImageViewSet
+from catalog.views import CatImageViewSet, proxy_cats_api
 
 router = DefaultRouter()
 router.register(r"cats", CatImageViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path('api/proxy/cats/', proxy_cats_api),
 ]
