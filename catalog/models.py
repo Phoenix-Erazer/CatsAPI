da07310a151e89d3a1c386122f6e6075cd4b7ac0
Phoen <Phoenixerazer@gmail.com>
2025-02-26 14:08:03 +0200

    add category in CatImage

--- catalog/models.py
+++ catalog/models.py
@@ -2,9 +2,21 @@ from django.db import models
 
 
 class CatImage(models.Model):
+    CATEGORY_CHOICES = [
+        ("cute", "Милый"),
+        ("funny", "Смешной"),
+        ("sleepy", "Сонный"),
+        ("playful", "Игривый"),
+    ]
+
     title = models.CharField(max_length=255)
     image_url = models.URLField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default="cute"
+    )
 
     def __str__(self):
         return self.title
